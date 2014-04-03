dc244663b1d31084d8682b0bc89cc61707ed7414
Greg Turnquist <gturnquist@gopivotal.com>
2014-04-03 15:09:38 -0500

    Fix spacing in code

--- complete/src/main/java/hello/Person.java
+++ complete/src/main/java/hello/Person.java
@@ -7,15 +7,16 @@ import org.springframework.data.gemfire.mapping.Region;
 @Region("hello")
 public class Person {
 
-    @Id public String name;
+    @Id
+    public String name;
     public int age;
 
     @PersistenceConstructor
     public Person(String name, int age) {
-        this.name = name; 
+        this.name = name;
         this.age = age;
     }
-    
+
     @Override
     public String toString() {
         return name + " is " + age + " years old.";
